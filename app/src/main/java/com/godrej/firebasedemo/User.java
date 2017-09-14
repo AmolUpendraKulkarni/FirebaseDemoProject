4f517abc433bccc716d4b61b1eb09f8c9b86ef10
AmolUpendraKulkarni <amolupendrakulkarni@gmail.com>
2017-09-14 16:20:04 +0530

    Initial commit

---
+++ app/src/main/java/com/godrej/firebasedemo/User.java
@@ -0,0 +1,37 @@
+package com.godrej.firebasedemo;
+
+/**
+ * Created by amolkulk on 7/20/2017.
+ */
+
+public class User {
+
+    public String username;
+    public String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
